0af7b1a03b2d47fc96e5e290d5a3410cc777a71d
Xutangzhi <1293815069@qq.com>
2021-11-23 22:11:04 +0800

    change two button names

---
+++ app/src/main/java/dev/dworks/apps/anexplorer/Takephoto.java
@@ -0,0 +1,14 @@
+package dev.dworks.apps.anexplorer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Takephoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_takephoto);
+    }
+}
